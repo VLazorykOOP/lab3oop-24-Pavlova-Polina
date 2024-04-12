f6551c1f6bbfa26791ad5b6167c9f39281ec41a0
Pavlova-Polina <pavlova.polina@chnu.edu.ua>
2024-04-12 10:44:39 +0300

    tasks

--- OOPLab3Tpl/Lab3Example.cpp
+++ OOPLab3Tpl/Lab3Example.cpp
@@ -4,103 +4,120 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include<Windows.h>
 #if !defined(CODING_VS_CODE)
 	#include <clocale>
+	
+
 #endif
 using namespace std;
 
-class Date {
-private:
-	int day;
-	int month;
-	int year;
-
-public:
-	Date() : day(1), month(1), year(2000) {}
-
-	Date(int d, int m, int y) {
-		if (isValidDate(d, m, y)) {
-			day = d;
-			month = m;
-			year = y;
-		}
-		else {
-			cout << "Некоректна дата. Буде застосовано значення за замовчуванням.\n";
-			day = 1;
-			month = 1;
-			year = 2000;
-		}
-	}
-
-	bool isValidDate(int d, int m, int y) {
-		if (m < 1 || m>12) return false;
-		if (d < 1 || d>31) return false;
-		if (m == 4 || m == 6 || m == 9 || m == 11) {
-			return(d <= 30);
-		}
-		else if (m == 2) {
-			if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
-				return (d <= 29);
-			}
-			else {
-				return (d <= 28);
-			}
-		}
-		return true;
-	}
-	void setDay(int d) {
-		if (isValidDate(d, month, year)) {
-			day = d;
-		}
-		else {
-			cout << "Некоректний день.Значення за замовченням.\n"
-		}
-	}
-	void setMonth(int m) {
-		if (isValidDate(day, m, year)) {
-			month = m;
-		}
-		else {
-			cout << "Некоректний місяць. Значення за замовченням.\n";
-		}
-	}
-	void setYear(int y) {
-		if (isValidDate(day, month, y)) {
-			year = y;
-		}
-		else {
-			cout << "Некоректний рік. Значення за замовченням.\n";
-		}
-	}
-
-	int getDay() const {
-		return day;
-	}
-
-	int getMonth() const {
-		return month;
-	}
-
-	int getYear() const {
-		return year;
-	}
-
-	void printformat1() const {
-		string months[12] = { "січня","лютого","березня","квітня","травня","червня","липня","серпня","вересня","жовтня","листопада","грудня" };
-		cout << day << " " << months[month - 1] << " " << year << " року\n";
-	}
-	void printformat2() const {
-		cout << (day < 10 ? "0" : "") << day << "." << (month < 10 ? "0" : "") << month << "." << year << endl;
-	}
-};
-int mainExample1()
-{
-	Date date1;
-	date1.printformat1();
-	date1.printformat2();
-
-}
-
+//class Date {
+//private:
+//	int day;
+//	int month;
+//	int year;
+//
+//public:
+//	Date() : day(1), month(1), year(2000) {}
+//
+//	Date(int d, int m, int y) {
+//		if (isValidDate(d, m, y)) {
+//			day = d;
+//			month = m;
+//			year = y;
+//		}
+//		else {
+//			cout << "Некоректна дата. Буде застосовано значення за замовчуванням.\n";
+//			day = 1;
+//			month = 1;
+//			year = 2000;
+//		}
+//	}
+//
+//	bool isValidDate(int d, int m, int y) {
+//		if (m < 1 || m>12) return false;
+//		if (d < 1 || d>31) return false;
+//		if (m == 4 || m == 6 || m == 9 || m == 11) {
+//			return(d <= 30);
+//		}
+//		else if (m == 2) {
+//			if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+//				return (d <= 29);
+//			}
+//			else {
+//				return (d <= 28);
+//			}
+//		}
+//		return true;
+//	}
+//	void setDay(int d) {
+//		if (isValidDate(d, month, year)) {
+//			day = d;
+//		}
+//		else {
+//			cout << "Некоректний день.Значення за замовченням.\n";
+//		}
+//	}
+//	void setMonth(int m) {
+//		if (isValidDate(day, m, year)) {
+//			month = m;
+//		}
+//		else {
+//			cout << "Некоректний місяць. Значення за замовченням.\n";
+//		}
+//	}
+//	void setYear(int y) {
+//		if (isValidDate(day, month, y)) {
+//			year = y;
+//		}
+//		else {
+//			cout << "Некоректний рік. Значення за замовченням.\n";
+//		}
+//	}
+//
+//	int getDay() const {
+//		return day;
+//	}
+//
+//	int getMonth() const {
+//		return month;
+//	}
+//
+//	int getYear() const {
+//		return year;
+//	}
+//
+//	void printformat1() const {
+//		string months[12] = { "січня","лютого","березня","квітня","травня","червня","липня","серпня","вересня","жовтня","листопада","грудня" };
+//		cout << day << " " << months[month - 1] << " " << year << " року\n";
+//	}
+//	void printformat2() const {
+//		cout << (day < 10 ? "0" : "") << day << "." << (month < 10 ? "0" : "") << month << "." << year << endl;
+//	}
+//};
+//int mainExample1()
+//{
+//	SetConsoleOutputCP(1251);
+//	Date date1;
+//	date1.printformat1();
+//	date1.printformat2();
+//
+//	Date date2(5, 1, 2019);
+//	date2.printformat1();
+//	date2.printformat2();
+//
+//	date2.setDay(32);
+//	date2.setMonth(13);
+//	date2.setYear(1900);
+//
+//	date2.printformat1();
+//	date2.printformat2();
+//
+//	return 0;
+//
+//}
+//
 
 // Ключове слово static 
 
@@ -299,177 +316,122 @@ int mainExample3()
 
 }
 /*example  4
-Створити тип даних - клас вектор, який має вказівник на ComplexDouble, число елементів і змінну стану. У класі визначити
+Створити тип даних - клас вектор, який має вказівник на int, число елементів і змінну стану. У класі визначити
 o	 конструктор без параметрів( виділяє місце для одного елемента та інінціалізує його в нуль);
 o	конструктор з одним параметром - розмір вектора( виділяє місце та інінціалізує масив значенням нуль);
 o	конструктор із двома параметрами - розмір вектора та значення ініціалізації(виділяє місце (значення перший аргумент) та інінціалізує значенням другого аргументу).
 o	конструктор копій та операцію присвоєння; // !!!
 o	деструктор звільняє пам'ять;
-o	визначити функції друку, додавання;
 У змінну стани встановлювати код помилки, коли не вистачає пам'яті, виходить за межі масиву. Передбачити можливість підрахунку числа об'єктів даного типу. Написати програму тестування всіх можливостей цього класу.
 */
 
-#include<complex>
-using namespace std;
-typedef complex<double> ComplexDouble;
-#define _RE 0
-#define _IM 1
+#include <iostream>
 
-class ComplexVector
-{
-	ComplexDouble* v;
-	int num;   // default num=2
-	int state = 0;
+class IntVector {
+	int* v;     // Вказівник на масив int
+	int num;    // Кількість елементів вектора
 public:
-	ComplexVector() : ComplexVector(2) {}
-	ComplexVector(int n);
-	ComplexVector(int n, ComplexDouble&);
-	ComplexVector(int n, ComplexDouble*);
-	ComplexVector(const ComplexVector& s);
-	ComplexVector& operator=(const ComplexVector& s);
-	~ComplexVector() {
-		std::cout << " del vec";
-		if (v) delete[] v;
-	}
-	void Output();
-	void Input();
-	ComplexVector Add(ComplexVector& b);
+	IntVector() : IntVector(2) {} // Конструктор без параметрів
+	IntVector(int n);             // Конструктор з одним параметром
+	IntVector(int n, int value);  // Конструктор з двома параметрами
+	IntVector(const IntVector& s); // Конструктор копіювання
+	IntVector& operator=(const IntVector& s); // Оператор присвоєння
+	~IntVector(); // Деструктор
 
+	void Output();  // Виведення вектора на екран
+	void Input();   // Введення вектора з клавіатури
+	IntVector Add(IntVector& b); // Додавання двох векторів
 };
 
-ComplexVector::ComplexVector(int n) {
-	if (n <= 0)    n = 2;  // default num =2;
+// Определення конструктора з одним параметром
+IntVector::IntVector(int n) {
+	if (n <= 0) n = 2;  // За замовчуванням, якщо n від'ємне або 0
 	num = n;
-	v = new ComplexDouble[n];
-	for (int i = 0; i < n; i++) {
-			v[i] = 0.0;
-			//v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;  
-		}
-
-}
-ComplexVector::ComplexVector(int n, ComplexDouble& b) : ComplexVector(n) {
-	for (int i = 0; i < num; i++) {
-		v[i] = b;
-		//v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;  
+	v = new int[n];     // Виділення пам'яті для масиву int
+	for (int i = 0; i < n; ++i) {
+		v[i] = 0;       // Ініціалізація елементів вектора нулями
 	}
 }
 
-ComplexVector::ComplexVector(int n, ComplexDouble* p) : ComplexVector(n) {
-	if (p != nullptr) 
-	for (int i = 0; i < num; i++) 
-		v[i] = p[i];
-	
+// Определення конструктора з двома параметрами
+IntVector::IntVector(int n, int value) {
+	if (n <= 0) n = 2;
+	num = n;
+	v = new int[n];
+	for (int i = 0; i < n; ++i) {
+		v[i] = value;   // Ініціалізація елементів вектора значенням value
+	}
 }
 
-ComplexVector::ComplexVector(const ComplexVector& s) {
-	// if (this == &s) return;  // the expression is used in the old standard
+// Определення конструктора копіювання
+IntVector::IntVector(const IntVector& s) {
 	num = s.num;
-	v = new ComplexDouble[num];
-	state = 0;
-	for (int i = 0; i < num; i++)   v[i] = s.v[i];
+	v = new int[num];
+	for (int i = 0; i < num; ++i) {
+		v[i] = s.v[i];  // Копіювання значень елементів з одного вектора в інший
+	}
 }
 
-ComplexVector& ComplexVector::operator=(const ComplexVector& s) {
-
-	if (num != s.num)
-	{
-		if (v) delete[] v;
-		num = s.num;
-		v = new ComplexDouble[num];
-		state = 0;
+// Определення оператора присвоєння
+IntVector& IntVector::operator=(const IntVector& s) {
+	if (this != &s) {  // Перевірка на самоприсвоєння
+		if (num != s.num) {
+			delete[] v; // Звільнення пам'яті попереднього масиву
+			num = s.num;
+			v = new int[num]; // Виділення нової пам'яті для копіювання
+		}
+		for (int i = 0; i < num; ++i) {
+			v[i] = s.v[i]; // Копіювання значень елементів
+		}
 	}
-	for (int i = 0; i < num; i++)   v[i] = s.v[i];
 	return *this;
 }
-void ComplexVector::Input() {
-	int in_num=0;
-		do {
-			cout << "Input size Vec\n";
-			cin >> in_num;
-		} while (in_num <= 0);
-		if (num != in_num ) {
-			num = in_num;
-			if (v) delete[] v;
-		v = new ComplexDouble[num];
-	}
-	for (int i = 0; i < num; i++) {
 
-#if defined(_MSC_VER)
-cout << " v [ " << i << " ] real img  "; cin >> v[i] >> v[i]._Val[_IM];
-#else 
-double re,im;
-cout << " v [ " << i << " ] real img  "; cin >> re>>im;
-v[i].real(re); 
-v[i].imag(im);
-#endif		
-		
-		
+// деструктор
+IntVector::~IntVector() {
+	if (v) delete[] v; // Звільнення виділеної пам'яті
+}
+
+// метод виведення вектора на екран
+void IntVector::Output() {
+	for (int i = 0; i < num; ++i) {
+		std::cout << " v [ " << i << " ]   " << v[i] << '\t' << std::endl;
 	}
 }
 
-void ComplexVector::Output() {
-	if (num != 0) {
-		for (int i = 0; i < num; i++) {
-			cout << " v [ " << i << " ]   " << v[i] << '\t';
-			cout << endl;
-		}
+// метод введення вектора з клавіатури
+void IntVector::Input() {
+	std::cout << "Input size of the vector: ";
+	std::cin >> num;
+	if (num <= 0) num = 2;
+	if (v) delete[] v;
+	v = new int[num];
+	for (int i = 0; i < num; ++i) {
+		std::cout << "Enter value for v[" << i << "]: ";
+		std::cin >> v[i];
 	}
 }
 
-ComplexVector ComplexVector::Add(ComplexVector& b) {
-	int tnum;
-	tnum = num < b.num ? num : b.num;
-	if (tnum >= 0) {
-		ComplexVector tmp(tnum);
-		for (int i = 0; i < tnum; i++) tmp.v[i] = v[i] + b.v[i];
-		return tmp;
+// метод додавання двох векторів
+IntVector IntVector::Add(IntVector& b) {
+	int tnum = (num < b.num) ? num : b.num;
+	IntVector tmp(tnum);
+	for (int i = 0; i < tnum; ++i) {
+		tmp.v[i] = v[i] + b.v[i]; // Додавання відповідних елементів
 	}
-	return ComplexVector(1);
+	return tmp;
 }
-
 
 int mainExample4()
 {
-	ComplexDouble a(1.0, 2), b, c;
-	cout << a << endl;
-#if defined(_MSC_VER)
-    b._Val[_RE] = 21.3;
-	b._Val[_IM] = 22.3;
-#else 
-    b.real( 21.3);
-	b.imag (22.3);
-#endif	
-	
-	cout << b << endl;
-	c = a + b;
-	cout << c << endl;
-	cout << " Test  " << endl;
-	ComplexVector VecObj, VecObj1(10);
-	cout << "VecObj \n";
-	VecObj.Output();
-	cout << "VecObj1 \n";
-	VecObj1.Output();
-	cout << " Input a " << endl;
-
-#if defined(_MSC_VER)
- cin >> a >> a._Val[_IM];
-#else 
-double re,im;
-cin >> re>>im;
-a.real(re); 
-a.imag(im);
-#endif		
-	cout << a << endl;
-	ComplexVector VecObj2(10, a);
-	VecObj2.Output();
-
-	VecObj.Input();
-	cout << endl;
-	VecObj.Output();
-	VecObj1 = VecObj.Add(VecObj2);
-	VecObj1.Output();
-
-	return 1;
+	IntVector VecObj, VecObj1(10); // Створення об'єктів IntVector
+	VecObj.Output(); // Виведення вектора VecObj на екран
+	VecObj1.Output(); // Виведення вектора VecObj1 на екран
+	VecObj.Input(); // Введення вектора VecObj з клавіатури
+	VecObj.Output(); // Виведення вектора VecObj на екран
+	VecObj1 = VecObj.Add(VecObj1); // Додавання двох векторів
+	VecObj1.Output(); // Виведення результату додавання на екран
+	return 0;
 }
 
 /// 
--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -2,23 +2,290 @@
 // виконання лаб. роботи №3. 
 // Він містинь функцію "main" з якої починається та закінчується виконання програми.
 //
-
 #include <iostream>
+#include <locale>
+using namespace std;
 
-// Ваші файли загловки 
-//
-#include "Lab3Example.h"
-int main()
-{
-    std::cout << " Lab #3  !\n";
-//  Код виконання завдань
-//  Головне меню завдань
-//  Функції та класи можуть знаходитись в інших файлах проекту
+class Date {
+private:
+    int m_day;
+    int m_month;
+    int m_year;
+public:
+    Date() {
+        m_day = 1;
+        m_month = 1;
+        m_year = 2000;
+    }
 
-    int conv = 1;
-    if (conv == 1) conv = mainExample1();
-    if (conv == 1) conv = mainExample3(); 
-    if (conv == 1) conv = mainExample4();
+    void set_day(int day) {
+        if (day > 31) {
+            cout << "Day isn't correct!" << endl;
+        }
+        else {
+            m_day = day;
+        }
+    }
+    Date(int day, int month) {
+        m_day = day;
+        m_month = month;
+        m_year = 2024;
+    }
+    void set_month(int month) {
+        if (month > 12) {
+            cout << "Month isn't correct!" << endl;
+        }
+        else {
+            m_month = month;
+        }
+    }
+    void set_year(int year) {
+        m_year = year;
+    }
+
+    void print_date() {
+        cout << m_day << "/" << m_month << "/" << m_year << endl;
+    }
+
+    void char_date() {
+        setlocale(LC_CTYPE, "ukr");
+        switch (m_month) {
+        case 1: cout << m_day << " Сiчня " << m_year << " Року " << endl; break;
+        case 2: cout << m_day << " Лютого " << m_year << " Року " << endl; break;
+        case 3: cout << m_day << " Березня " << m_year << " Року " << endl; break;
+        case 4: cout << m_day << " Квiтня " << m_year << " Року " << endl; break;
+        case 5: cout << m_day << " Травня " << m_year << " Року " << endl; break;
+        case 6: cout << m_day << " Червня " << m_year << " Року " << endl; break;
+        case 7: cout << m_day << " Липня " << m_year << " Року " << endl; break;
+        case 8: cout << m_day << " Серпня " << m_year << " Року " << endl; break;
+        case 9: cout << m_day << " Вересня " << m_year << " Року " << endl; break;
+        case 10: cout << m_day << " Жовтня " << m_year << " Року " << endl; break;
+        case 11: cout << m_day << " Листопада " << m_year << " Року " << endl; break;
+        case 12: cout << m_day << " Грудня " << m_year << " Року " << endl; break;
+        }
+    }
+
+};
+
+void task1() {
+    int day, month, year;
+    Date object, object2(2, 4);
+    cout << "Enter date" << endl;
+    cin >> day;
+    cin >> month;
+    cin >> year;
+    object.set_day(day);
+    object.set_month(month);
+    object.set_year(year);
+    object.print_date();
+    object.char_date();
+    object2.print_date();
+    object2.char_date();
 
 }
+#include <iostream>
+using namespace std;
+class Vector {
+private:
+    int* data;
+    int size;
+    int& state;
+
+public:
+    Vector() : data(new int[1]), size(1), state(count) {                  //конструктор без параметрів
+        data[0] = 0;
+        ++count;
+    }
+    Vector(int s);             // Конструктор з одним параметром
+    Vector(int s, int init_value);  // Конструктор з двома параметрами
+    Vector(const Vector& other); // Конструктор копіювання
+    Vector& operator=(const Vector& other); // Оператор присвоєння
+    ~Vector() {                            //деструктор
+        delete[] data;
+        --count;
+    }
+    void Output();  // Виведення вектора на екран
+    void Input();   // Введення вектора з клавіатури
+    Vector Add(const Vector& other); // Додавання двох векторів
+    Vector substract(const Vector& other); //Віднімання
+    Vector multiply(short factor); // Множення
+    void setElement(int index, int value = 0); //Присвоєння значення елементу
+    void setElement(int index, int value = 0); //Присвоєння значення елементу
+    void print(); //друк
+    static int count;
+
+
+    int getElement(int index) const {                                //функція яка одержує деякий елемент масиву
+        if (index >= 0 && index < size) {
+            return data[index];
+        }
+        else {
+            state = -1;                                            //помилка
+            return 0;
+        }
+    }
+
+    bool operator>(const Vector& other) const {                                    //ф-ція порівняння більше, менше або рівно
+        if (size != other.size) {
+            return false;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (data[i] <= other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool operator<(const Vector& other) const {
+        if (size != other.size) {
+            return false;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (data[i] >= other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool operator==(const Vector& other) const {
+        if (size != other.size) {
+            return false;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (data[i] != other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+int Vector::count = 0;                           //скидання лічильника 
+
+Vector::Vector(int s) : data(new int[s]), size(s), state(count) {                 //конструктор з одним параметром ( розмір вектора)
+    ++count;
+    for (int i = 0; i < size; ++i) {
+        data[i] = 0;
+    }
+}
+
+Vector::Vector(int s, int init_value) : data(new int[s]), size(s), state(count) {            //конструктор з двома параметрами 
+    ++count;
+    for (int i = 0; i < size; ++i) {
+        data[i] = init_value;
+    }
+}
+
+Vector::Vector(const Vector& other) : data(new int[other.size]), size(other.size), state(count) {          //конструктор копій
+    ++count;
+    for (int i = 0; i < size; ++i) {
+        data[i] = other.data[i];
+    }
+}
+
+Vector& Vector::operator=(const Vector& other) {                //операція присвоєння
+    if (this == &other)
+        return *this;
+    delete[] data;
+    size = other.size;
+    data = new int[size];
+    for (int i = 0; i < size; ++i) {
+        data[i] = other.data[i];
+    }
+    return *this;
+}
+
+Vector Vector::Add(const Vector& other) {                                         //ф-ція додавання
+    if (size != other.size) {
+        Vector result;
+        result.state = -2;                                                     //помилка
+        return result;
+    }
+    Vector result(size);
+    for (int i = 0; i < size; ++i) {
+        result.data[i] = data[i] + other.data[i];
+    }
+    return result;
+}
+
+Vector Vector::substract(const Vector& other) {                                     //ф-ція віднімання
+    if (size != other.size) {
+        Vector result;
+        result.state = -2;                                          //помилка
+        return result;
+    }
+    Vector result(size);
+    for (int i = 0; i < size; ++i) {
+        result.data[i] = data[i] - other.data[i];
+    }
+    return result;
+}
+
+Vector Vector::multiply(short factor) {                                 //множення типу short
+    Vector result(size);
+    for (int i = 0; i < size; ++i) {
+        result.data[i] = data[i] * factor;
+    }
+    return result;
+}
+
+void Vector::setElement(int index, int value = 0) {                       //функція, яка присвоює елементу  деяке значення
+    if (index >= 0 && index < size) {
+        data[index] = value;
+    }
+    else {
+        state = -1; // помилка
+    }
+}
+void Vector::print() {                                              //ф-ція друку
+    cout << "[";
+    for (int i = 0; i < size; ++i) {
+        cout << data[i];
+        if (i != size - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+
+int task2() {
+    setlocale(LC_CTYPE, "Ukr");
+    //тест класу "Вектор"
+    Vector vec1;                                             // використання конструктора без параметрів
+    vec1.print();
+
+    Vector vec2(5);                                           // використання конструктора з одним параметром
+    vec2.print();
+
+    Vector vec3(3, 7);                                        // використання конструктора з двома параметрами(створює три ел. із значенням 7)
+    vec3.print();
+
+    Vector vec4 = vec3;                                       // використання конструктора копій(створює копію vec3)
+    vec4.print();
+
+    Vector vec5;
+    vec5 = vec2;                                               // використання операції присвоєння(присвоює vec5 = vec2)
+    vec5.print();
+
+    cout << "Кількість об*єктів класу Vector: " << Vector::count << endl;
+    return 0;
+}
+int main()
+{
+    int choice;
+    cout << "Choose task" << endl;
+    cout << "1.Task 1 (with class Date)" << endl;
+    cout << "2.Task 2 (with class Vector)" << endl;
+    cout << "3.Exit" << endl;
+
+    cin >> choice;
+
+    switch (choice) {
+    case 1: task1(); break;
+    case 2: task2(); break;
+    case 3: return 0;
+    default: cout << "Error" << endl;
+    }
+}
+
 
